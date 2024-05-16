29c225c8c75827de47d59d21ff65c78d56eb5daa
miroslavholec <mirek@miroslavholec.cz>
2024-05-16 15:36:59 +0200

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0NjU3MQ/spectral.js?branch_slug=main&token=f2062bd7-6e07-4b49-a2d4-6cd89b7ce376";
+export default { extends: ruleset };
